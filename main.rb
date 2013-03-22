196400441b9be53b6a818de39301e6b42d318833
itchallenge <itchallenge@localhost.localdomain>
2013-03-23 00:26:54 +0900

    FileScoreManagerでやるように暫定的に設定

--- main.rb
+++ main.rb
@@ -1,8 +1,10 @@
 
-$LOAD_PATH << (File.dirname(__FILE__)) 
+pwd = File.dirname(__FILE__)
+$LOAD_PATH << pwd
 require "readline"
 require 'Quiz/CsvQuestion.rb'
-require 'Quiz/NullScoreManager.rb'
+require 'Quiz/FileScoreManager.rb'
+
 
 # Username Check
 
@@ -13,18 +15,27 @@ if(username == '')
 		Readline.readline
 	end
 
+#q = CsvQuestion.new(pwd + '/data/css-proplist.csv').q;
+#q = CsvQuestion.new('data/css-value.csv').q;
 q = CsvQuestion.new('keywords.csv').q;
-results = [];
+results = []
+
+starttime = Time.now 
 
 q.each{|current|
 	puts current['Question']
 	print 'input:'
 	input = Readline.readline 
-	result = current['Answer'] == input ? true : false;
+	result = current['Answer'] == input ? true : false
 	results.push({current['Question'] => result}) 
 }
+endtime = Time.now
+totaltime = (endtime - starttime).to_i 
 
-score = NullScoreManager.new(results,username)
+#score = NullScoreManager.new(username,results,totaltime)
+score = FileScoreManager.new('/tmp/tmp',username,results,totaltime)
 
 
 puts score.username + "'s Result: " + score.score.to_s + ' of ' + score.questions.to_s + ' (' + (score.score.to_f / score.questions.to_f * 100).to_s + '%)'
+
+puts totaltime.to_s + ' seconds'
